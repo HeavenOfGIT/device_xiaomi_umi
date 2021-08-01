2f36d29e5163fe16bf2d09ac3c956ff085c0274a
Adrian Lam <adrianlam230@gmail.com>
2021-08-01 21:14:34 +0800

    umi: REvert

--- kona/audio/audio.mk
+++ kona/audio/audio.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
     libqahwwrapper \
     libqahw \
     android.hardware.bluetooth.audio@2.0-impl \
-    libbluetooth_qti \
+    audio.bluetooth.default \
     liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
@@ -116,11 +116,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
     
 # Low latency audio buffer size in frames
 PRODUCT_PROPERTY_OVERRIDES += \

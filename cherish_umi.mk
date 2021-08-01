fa8d5e840f1a12b8dce6c18dcb2019516222385a
Adrian Lam <adrianlam230@gmail.com>
2021-08-01 20:04:16 +0800

    umi: Included live wallpapaer

--- cherish_umi.mk
+++ cherish_umi.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/cherish/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := cherish_umi

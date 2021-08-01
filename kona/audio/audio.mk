3609a3caff8124d2ede087ab6377dff3fc874b2f
Adrian Lam <adrianlam230@gmail.com>
2021-08-01 11:49:32 +0800

    umi: Switch to QTI hal

--- kona/audio/audio.mk
+++ kona/audio/audio.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
     libqahwwrapper \
     libqahw \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
+    libbluetooth_qti \
     liba2dpoffload \
     libaudiopreprocessing \
     libbatterylistener \
@@ -116,9 +116,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     
 # Low latency audio buffer size in frames
 PRODUCT_PROPERTY_OVERRIDES += \

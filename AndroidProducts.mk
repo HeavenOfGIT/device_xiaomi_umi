9e300d6b8ef8e24bc76726f07057abd37777d0d7
Adrian Lam <adrianlam230@gmail.com>
2021-08-03 13:17:32 +0800

    Revert "Merge branch 'KangOS_OSS' into CherishOS"
    This reverts commit 2a78d0610c0a86146f7e5ced33fbb4a768f6fdfc, reversing
    changes made to 71d0b563b57dfdd14500af08be7b5604a3495d15.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,9 +3,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
- $(LOCAL_DIR)/kangos_umi.mk
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/cherish_umi.mk
 
 COMMON_LUNCH_CHOICES := \
-    kangos_umi-user \
-    kangos_umi-userdebug \
-    kangos_umi-eng
+    cherish_umi-user \
+    cherish_umi-userdebug \
+    cherish_umi-eng

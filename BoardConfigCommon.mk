8cfbca051c92dc02578585c1ca78d5433943d4c2
Adrian Lam <adrianlam230@gmail.com>
2021-08-03 22:00:56 +0800

    umi: BC fix sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,7 +170,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
-include COMMON/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
